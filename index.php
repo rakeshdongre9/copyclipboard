c6cc00d9d2bf087e5def351690a202c22e97be2e
RAKESH DONGRE <112848752+rakeshdongre9@users.noreply.github.com>
2024-06-25 22:01:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+             <div class="col-12 grid-margin">
+                <div class="card">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="card-body">
+                        <h4 class="card-title">clipboard on text input</h4>
+                        <p class="card-description">Cut/copy from text input</p>
+                        <input type="text" id="clipboardExample1" class="form-control" value="hello">
+                        <div class="mt-3">
+                          <button type="button" class="btn btn-info btn-clipboard" data-clipboard-action="copy" data-clipboard-target="#clipboardExample1">Copy</button>
+                          <button type="button" class="btn btn-success btn-clipboard" data-clipboard-action="cut" data-clipboard-target="#clipboardExample1">Cut</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="card-body">
+                        <h4 class="card-title">Clipboard on textarea</h4>
+                        <p class="card-description">Cut/copy from textarea</p>
+                        <textarea id="clipboardExample2" class="form-control" rows="3">Hello</textarea>
+                        <div class="mt-3">
+                          <button type="button" class="btn btn-info btn-clipboard" data-clipboard-action="copy" data-clipboard-target="#clipboardExample2">Copy</button>
+                          <button type="button" class="btn btn-success btn-clipboard" data-clipboard-action="cut" data-clipboard-target="#clipboardExample2">Cut</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                                 
+                                 
+                           
